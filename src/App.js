19a0bea6d22f6a920e1337a4c8597c232b853923
Minseok Kim <mskim@ridi.com>
2018-12-01 18:26:57 +0900

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+
+
+const API_BASE_URL = 'https://hv453sl4wh.execute-api.ap-northeast-2.amazonaws.com/production'
+
+class App extends Component {
+
+  id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+
+  state = {
+    input: '',
+    todos: []
+  }
+  updateTodos = () => {
+	fetch(`${API_BASE_URL}/todos`).then(response => response.json()).then(todos => {
+		this.setState({
+			todos
+		})
+	})
+  }
+
+  componentDidMount() {
+	  // Call get todos
+	  this.updateTodos = this.updateTodos.bind(this)
+
+	  this.updateTodos();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value // input 의 다음 바뀔 값
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+	fetch(`${API_BASE_URL}/todos`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({text: input})
+	}).then(response => response.json()).then(response => {
+		if (response.success === true){
+			this.updateTodos();
+		}
+	});
+  }
+
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 면 handleCreate 호출
+    if(e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+  handleToggle = (id) => {
+	// Call PUT todo
+    const { todos } = this.state;
+
+    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; // 선택한 객체
+
+    const nextTodos = [...todos]; // 배열을 복사
+
+    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+  }
+
+  handleRemove = (id) => {
+	// Call DELETE todo
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  render() {
+    const { input, todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove
+    } = this;
+
+    return (
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+      </TodoListTemplate>
+	);
+  }
+}
+
+export default App;
